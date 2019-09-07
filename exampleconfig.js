0fe32bfd4bca4050931ca22dd4d863c85faaadad
Mark Cabanero <me@markcabanero.com>
2019-09-07 10:27:41 -0700

    Lint files

--- exampleconfig.js
+++ exampleconfig.js
@@ -1,7 +1,7 @@
-//Insert your Twitter app's keys/tokens then rename the file to config.js
+// Insert your Twitter app's keys/tokens then rename the file to config.js
 module.exports = {
-    consumer_key:         '',
-    consumer_secret:      '',
-    access_token:         '',
-    access_token_secret:  ''
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 }
--- test.js
+++ test.js
@@ -1,50 +1,46 @@
-const config    = require('./config.js');
-const Twit      = require('twit');
-const CronJob   = require('cron').CronJob;
-const T = new Twit(config);
+const config = require('./config.js')
+const Twit = require('twit')
+const CronJob = require('cron').CronJob
+const T = new Twit(config)
 
-const maxDays = 112;
-const oneDay = 1000 * 60 * 60 * 24; //milliseconds in a day
-const semesterEndDate = new Date('December 15, 2019, 16:00:00');
+const maxDays = 112
+const oneDay = 1000 * 60 * 60 * 24 // milliseconds in a day
+const semesterEndDate = new Date('December 15, 2019, 16:00:00')
 
+// Cronjob to initiate SendTweet function at 8 AM every day
+new CronJob('0 11 * * *', function () {
+  SendTweet()
+}, null, true, 'EST')
 
-//Cronjob to initiate SendTweet function at 8 AM every day
-new CronJob('0 11 * * *', function() {
-    SendTweet();
-}, null, true, 'EST');
+// Create and post the progress tweet
+function SendTweet () {
+  const currentDate = new Date()
+  const diff = currentDate - semesterEndDate
+  const currentDay = Math.floor(diff / oneDay)
+  const currentPercent = 100 + currentDay / maxDays * 100
 
-//Create and post the progress tweet
-function SendTweet() {
-    let currentDate = new Date();
-    let diff = currentDate - semesterEndDate;
-    let currentDay = Math.floor(diff / oneDay);
-    let currentPercent = 100 + currentDay / maxDays * 100;
+  const tweet = CreateBar(currentPercent) + ' ' + currentPercent.toFixed(2) + '% of the UConn semester has passed!'
 
-    const tweet = CreateBar(currentPercent) + ' ' + currentPercent.toFixed(2) + '% of the UConn semester has passed!';
+  T.post('statuses/update', { status: tweet }, tweeted)
 
-    T.post('statuses/update', { status: tweet }, tweeted);
-
-    // Callback for when the tweet is sent
-    function tweeted(err, data, response) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('Success: ' + data.text);
-        }
+  // Callback for when the tweet is sent
+  function tweeted (err, data, response) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Success: ' + data.text)
     }
+  }
 }
 
 /**
  * @return {string}
  */
-function CreateBar(percent) {
-    let yearBar = '';
-    for(let i = 5; i < 100; i += 5) {
-        yearBar = (i < percent) ? yearBar + '▓' : yearBar + '░';
-    }
+function CreateBar (percent) {
+  let yearBar = ''
+  for (let i = 5; i < 100; i += 5) {
+    yearBar = (i < percent) ? yearBar + '▓' : yearBar + '░'
+  }
 
-    return yearBar
+  return yearBar
 }
-
-
-
